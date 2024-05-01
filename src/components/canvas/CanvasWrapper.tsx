d9bb4fb0c789025fbb660746b47e480fd10c5da3
admon84 <admonwest84@gmail.com>
2024-05-01 15:22:42 -0600

    feat: experimenting with @screeps/renderer

---
+++ src/components/canvas/CanvasWrapper.tsx
@@ -0,0 +1,23 @@
+import React, { useState, useCallback } from 'react';
+import * as Mui from '@mui/material';
+import Canvas from './Canvas';
+import CanvasDebugPanel from './CanvasDebugPanel';
+import { EXAMPLE_STRUCTURES, EXAMPLE_TERRAIN } from '../../utils/constants';
+import type { Metrics } from '@/types';
+
+export default function CanvasWrapper() {
+  const [metrics, setMetrics] = useState<Metrics>({
+    fps: 0,
+  });
+
+  const handleMetricsUpdate = useCallback((newMetrics: Metrics) => {
+    setMetrics((prevMetrics) => ({ ...prevMetrics, ...newMetrics }));
+  }, []);
+
+  return (
+    <>
+      <Canvas samples={EXAMPLE_STRUCTURES} terrain={EXAMPLE_TERRAIN} onMetricsUpdate={handleMetricsUpdate} />
+      <CanvasDebugPanel metrics={metrics} />
+    </>
+  );
+}
