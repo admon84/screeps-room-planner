4cb76b5609adb45713045a77739bcaaedcaa565a
admon84 <admonwest84@gmail.com>
2024-05-03 15:03:04 -0600

    wip: add ability to place different structures on the map and refactoring hooks for game state

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,9 @@
 import * as Constants from './constants';
 import { Point } from '../types';
 
+export const isRoomPosition = (x: number, y: number) =>
+  x >= 0 && x < Constants.ROOM_SIZE && y >= 0 && y < Constants.ROOM_SIZE;
+
 export const getDistance = (a: Point, b: Point) => Math.max(Math.abs(b.x - a.x), Math.abs(b.y - a.y));
 
 export const getTile = (x: number, y: number) => y * Constants.ROOM_SIZE + x;
@@ -26,9 +29,6 @@ export const getTileForShort = (shortPoint: string) => {
   const { x, y } = getPointForShort(shortPoint);
   return getTile(x, y);
 };
-
-export const positionIsValid = (x: number, y: number) =>
-  x >= 0 && x < Constants.ROOM_SIZE && y >= 0 && y < Constants.ROOM_SIZE;
 
 export const getStructureProps = (key: string, rcl = Constants.MAX_RCL) => ({
   key,
--- src/utils/worldConfigs.ts
+++ src/utils/worldConfigs.ts
@@ -1,17 +1,23 @@
 import '@screeps/renderer-metadata';
+import { USER_ID } from './constants';
+
+export const ATTACK_PENETRATION = 10;
+export const CELL_SIZE = 100;
+export const VIEW_BOX = 5000;
+export const RENDER_SIZE = {
+  width: 2048,
+  height: 2048,
+};
 
 export const worldConfigs = {
-  ATTACK_PENETRATION: 10,
-  CELL_SIZE: 100,
-  RENDER_SIZE: {
-    width: 2048,
-    height: 2048,
-  },
-  VIEW_BOX: 5000,
+  ATTACK_PENETRATION,
+  CELL_SIZE,
+  RENDER_SIZE,
+  VIEW_BOX,
   BADGE_URL: 'https://screeps.com/api/user/badge-svg?username=%1',
   metadata: RENDERER_METADATA,
   gameData: {
-    player: '561e4d4645f3f7244a7622e8',
+    player: USER_ID,
     showMyNames: {
       spawns: true,
       creeps: true,
